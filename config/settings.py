0206c22f2a12f9721d9b15fdfb04778633853960
Ahmed Elghareeb <me@rancho.me>
2024-08-19 19:43:53 +0200

    feat: adds django apps to installed apps

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # project apps
+    "apps.blog",
+    "apps.events",
+    "apps.news",
+    "apps.scribes",
+    "apps.handwriting",
+    "apps.manuscripts",
 ]
 
 MIDDLEWARE = [
