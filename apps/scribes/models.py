7452260f57f4ab14ea6fb6e59b0005a0b86c753c
Ahmed Elghareeb <me@rancho.me>
2024-08-20 21:35:09 +0200

    feat: adds scribes models

--- apps/scribes/models.py
+++ apps/scribes/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Scribe(models.Model):
+    name = models.CharField(max_length=100)
+    period = models.CharField(max_length=100)
+    scriptorium = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Script(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Hand(models.Model):
+    scribe = models.ForeignKey(Scribe, on_delete=models.PROTECT)
+    script = models.ForeignKey(Script, on_delete=models.PROTECT)
+
+    name = models.CharField(max_length=100)
+    item_part = models.ForeignKey("manuscripts.ItemPart", on_delete=models.PROTECT)
+    date = models.CharField(max_length=100)
+    place = models.CharField(max_length=100)
+
+    description = models.TextField()
+    item_part_images = models.ManyToManyField(
+        "manuscripts.ItemPart",
+        through="HandToItemPartImages",
+        related_name="hands",
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class HandToItemPartImages(models.Model):
+    hand = models.ForeignKey(Hand, on_delete=models.PROTECT)
+    item_part = models.ForeignKey("manuscripts.ItemPart", on_delete=models.PROTECT)
