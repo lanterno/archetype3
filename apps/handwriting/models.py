cdbddfcf62a1a792e4cc1cf8e23d7b6da886f744
Ahmed Elghareeb <me@rancho.me>
2024-09-17 19:27:28 +0200

    feat: adds handwriting models

--- apps/handwriting/models.py
+++ apps/handwriting/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Character(models.Model):
+    class CharacterForm(models.TextChoices):
+        MAJUSCULE = "Majuscule"
+        MINUSCULE = "Minuscule"
+        NUMERAL = "Numeral"
+        PUNCTUATION = "Punctuation"
+        SYMBOL = "Symbol"
+
+    name = models.CharField(max_length=10)
+    form = models.CharField(choices=CharacterForm.choices, max_length=11, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Allograph(models.Model):
+    character = models.ForeignKey(Character, on_delete=models.CASCADE)
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class Graph(models.Model):
+    item_image = models.ForeignKey("manuscripts.ItemImage", on_delete=models.CASCADE)
+    location = models.JSONField()
+    allograph = models.ForeignKey(Allograph, on_delete=models.CASCADE)
+
+
+class Feature(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+class Component(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    features = models.ManyToManyField(Feature)
