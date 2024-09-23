148548d2e505549e816a80a702ade9e6faf446ec
Ahmed Elghareeb <me@rancho.me>
2024-09-23 17:50:26 +0200

    feat: uses postgreSQL

--- config/settings.py
+++ config/settings.py
@@ -84,14 +84,14 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    ),
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
