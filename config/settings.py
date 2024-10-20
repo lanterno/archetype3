afe6d204f85956d0e9b65e42a02de1c69a3c0c2f
Ahmed Elghareeb <me@rancho.me>
2024-10-20 22:42:02 +0200

    feat: enables pagination in List APIs

--- config/settings.py
+++ config/settings.py
@@ -144,6 +144,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework.authentication.TokenAuthentication",),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 20,
 }
 
 AUTH_USER_MODEL = "users.User"
